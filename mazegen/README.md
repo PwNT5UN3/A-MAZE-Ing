359aa35eba9ddf8e859f1498074e373e71bfa94e
Maximilian Felix Welsch <mawelsch@student.42heilbronn.de>
2026-01-23 14:59:11 +0100

    made mazegen packagable with included readme

---
+++ mazegen/README.md
@@ -0,0 +1,1 @@
+v bcsd hbsa fcgbhksdvcfbjsdb csb 
--- mazegen/__init__.py
+++ mazegen/__init__.py
@@ -1,5 +1,6 @@
-from mazegen.maze_gen import MazeCell, DFSearch
-from mazegen.maze_gen import MazeGenerator
-from mazegen.maze_gen import WilsonsAlgorithm
+from .maze_gen import *
+from .bfs import *
 
-__all__ = ["MazeCell", "DFSearch", "MazeGenerator", "WilsonsAlgorithm"]
+__all__ = ["MazeCell", "DFSearch", "MazeGenerator", "WilsonsAlgorithm", "BFS"]
+__version__ = "1.0.0"
+__author__ = "Matvii Nesterenko, Max Welsch"
